e548b074a2d835a3996edaeaff78f2a7582fe831
parsentev <parsentev@yandex.ru>
2023-11-19 15:33:23 +0300

    Drop setId

--- src/main/java/org/springframework/test/fake/CrudRepositoryFake.java
+++ src/main/java/org/springframework/test/fake/CrudRepositoryFake.java
@@ -10,64 +10,64 @@ public abstract class CrudRepositoryFake<T extends Base<ID>, ID> implements Crud
     protected final Map<ID, T> memory = new HashMap<>();
 
     @Override
-    public final <S extends T> S save(S entity) {
+    public <S extends T> S save(S entity) {
         memory.put(entity.getId(), entity);
         return entity;
     }
 
     @Override
-    public final <S extends T> Iterable<S> saveAll(Iterable<S> entities) {
+    public <S extends T> Iterable<S> saveAll(Iterable<S> entities) {
         entities.forEach(this::save);
         return entities;
     }
 
     @Override
-    public final Optional<T> findById(ID id) {
+    public Optional<T> findById(ID id) {
         return Optional.ofNullable(memory.get(id));
     }
 
     @Override
-    public final boolean existsById(ID id) {
+    public boolean existsById(ID id) {
         return memory.containsKey(id);
     }
 
     @Override
-    public final Iterable<T> findAll() {
+    public Iterable<T> findAll() {
         return memory.values();
     }
 
     @Override
-    public final Iterable<T> findAllById(Iterable<ID> ids) {
+    public Iterable<T> findAllById(Iterable<ID> ids) {
         throw new UnsupportedOperationException("Not impl yet.");
     }
 
     @Override
-    public final long count() {
+    public long count() {
         return memory.size();
     }
 
     @Override
-    public final void deleteById(ID id) {
+    public void deleteById(ID id) {
         memory.remove(id);
     }
 
     @Override
-    public final void delete(T entity) {
+    public void delete(T entity) {
         memory.remove(entity.getId());
     }
 
     @Override
-    public final void deleteAllById(Iterable<? extends ID> ids) {
+    public void deleteAllById(Iterable<? extends ID> ids) {
         throw new UnsupportedOperationException("Not impl yet.");
     }
 
     @Override
-    public final void deleteAll(Iterable<? extends T> entities) {
+    public void deleteAll(Iterable<? extends T> entities) {
         throw new UnsupportedOperationException("Not impl yet.");
     }
 
     @Override
-    public final void deleteAll() {
+    public void deleteAll() {
         memory.clear();
     }
 }
